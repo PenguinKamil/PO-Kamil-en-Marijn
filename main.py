df7fd0a2647018194dcdd2355861d85cff3706ec
Kamil Marsman <97417228+PenguinKamil@users.noreply.github.com>
2022-06-15 07:46:49 +0000

    Nothing, second commit

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+    return 'Hello from the depths of Uk!'
 
-app.run(host='0.0.0.0', port=81)
+
+app.run(host='0.0.0.0', port=81, true)
